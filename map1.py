dca25098e80d8d86289b99a98f4473220dd9fc06
splendidabhi <abhi08071999@gmail.com>
2021-01-22 11:21:05 +0530

    added volcanoes point

--- map1.py
+++ map1.py
@@ -1,7 +1,15 @@
 import folium
-map = folium.Map(location=[38.58,-99.09],zoom_start=6,titles="Mapbox Bright")
+import pandas
+data = pandas.read_csv("valconoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
 
-fg.add_child(folium.Marker(location=[38.2,-99.1],popup="Hi I am Marker",icon=folium.Icon(color='green')))
+
+map = folium.Map(location=[38.58,-99.09],zoom_start=6,titles="Mapbox Bright")
+fg = folium.FeatureGroup(name="My Map")
+
+for lt,ln in zip(lat,lon):
+    fg.add_child(folium.Marker(location=[lt,ln],popup="Hi I am Marker",icon=folium.Icon(color='green')))
 map.add_child(fg)
 
 map.save("Map1.html")
