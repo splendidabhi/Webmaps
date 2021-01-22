aa08e4fb6b9017ba4263c7a839ee0458c464578d
splendidabhi <abhi08071999@gmail.com>
2021-01-22 18:04:12 +0530

    different color for different elevation

--- map1.py
+++ map1.py
@@ -1,15 +1,23 @@
 import folium
 import pandas
 data = pandas.read_csv("valconoes.txt")
-lat = list(data["LAT"])
-lon = list(data["LON"])
+lat = list(data["LAT"])     #latitude
+lon = list(data["LON"])     #longitude
+elev = list(data["ELEV"])   #elevation
 
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <=elevation <= 2500:
+        return 'orange'
+    else:
+        return 'red'
 
 map = folium.Map(location=[38.58,-99.09],zoom_start=6,titles="Mapbox Bright")
 fg = folium.FeatureGroup(name="My Map")
 
-for lt,ln in zip(lat,lon):
-    fg.add_child(folium.Marker(location=[lt,ln],popup="Hi I am Marker",icon=folium.Icon(color='green')))
+for lt,ln,el in zip(lat,lon,elev):
+    fg.add_child(folium.CircleMarker(location=[lt,ln],radius = 6 ,popup=str(el)+" m",fill_color = color_producer(el),color = 'grey',fill_opacity=0.7))
 map.add_child(fg)
 
 map.save("Map1.html")
