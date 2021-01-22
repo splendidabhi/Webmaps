14ce5302a4a2fdc531845f66ca0a87e505440d80
splendidabhi <abhi08071999@gmail.com>
2021-01-22 10:29:52 +0530

    Added a point marker feature to the webmap

--- map1.py
+++ map1.py
@@ -1,3 +1,8 @@
 import folium
 map = folium.Map(location=[38.58,-99.09],zoom_start=6,titles="Mapbox Bright")
+
+fg = folium.FeatureGroup(name="My Map")
+fg.add_child(folium.Marker(location=[38.2,-99.1],popup="Hi I am Marker",icon=folium.Icon(color='green')))
+map.add_child(fg)
+
 map.save("Map1.html")
